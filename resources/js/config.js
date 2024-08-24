f260b062e6ba3f3260a65bf0544518ebb76a3f5d
Carl Anthony Magumpara <carl@lexmeet.com>
2024-08-24 19:16:21 +0800

    updates

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,1 +1,5 @@
-export const baseUrl = 'http://localhost:8000/api';
+function isDev() {
+  return (!process.env.NODE_ENV || process.env.NODE_ENV === 'development');
+}
+
+export const baseUrl = isDev() ? 'http://localhost:8000/api' : 'https://amdl.carlmagumpara.tech/api';
